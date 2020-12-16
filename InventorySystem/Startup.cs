7706d04d66ecc6d5aef91d4e2ecd1201e18d4bb8
Antonio Nazco Menendez <tonito.nazco@gmail.com>
2020-12-15 20:58:40 -0500

    Setting Razor Runtime Compilation at InventorySystem

--- InventorySystem/Startup.cs
+++ InventorySystem/Startup.cs
@@ -33,7 +33,8 @@ namespace InventorySystem
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
 
